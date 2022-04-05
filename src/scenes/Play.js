e6a1458e2e0f98ff09e3a0503010e2c0ab69cae0
Jessica <jhuan221@ucsc.edu>
2022-04-05 13:57:18 -0700

    implemented scrolling starfield and started rocket class

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,7 +3,16 @@ class Play extends Phaser.Scene{
         super("playScene");
     }
 
+    preload() {
+        // load images/tile sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
     create() {
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding,
         game.config.width, borderUISize * 2,
@@ -18,5 +27,11 @@ class Play extends Phaser.Scene{
         this.add.rectangle(game.config.width -borderUISize, 0, borderUISize,
         game.config.height, 0xFFFFFF).setOrigin(0,0);
     }
+
+    update() {
+        this.starfield.tilePositionX -= 4;
+    }
 }
+
+
 
